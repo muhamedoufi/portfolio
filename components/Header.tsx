f1b00899d7e724c0113a27170a6891ce5bb2f3a4
Mouhamedou <muhamedoufi@gmail.com>
2024-11-09 16:58:35 +0000

    commi

---
+++ components/Header.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Nav from "./Nav";
+import { Button } from "./ui/button";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white bg-pink-50/20">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Muhamedou <span className="text-accent">fi.</span>
+          </h1>
+        </Link>
+        {/* Desktop Nav */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link className="" href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+        {/* Mobile Nav  */}
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ tailwind.config.ts
@@ -0,0 +1,59 @@
+import type { Config } from "tailwindcss";
+import { DEFAULT_CIPHERS } from "tls";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960x",
+      xl: "1200px",
+      "2xl": "1400px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#00ff99",
+          hover: "#00e187",
+        },
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
+  },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config;
